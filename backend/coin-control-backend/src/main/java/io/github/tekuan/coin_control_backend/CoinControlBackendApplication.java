824716b469249679edacadbba28981f437744b86
Larissa Bianca <larissa_bianca@outlook.com>
2025-03-25 22:05:47 -0300

    Adicionando arquivos iniciais do spring boot initializer

---
+++ backend/coin-control-backend/src/main/java/io/github/tekuan/coin_control_backend/CoinControlBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.tekuan.coin_control_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinControlBackendApplication.class, args);
+	}
+
+}
---
+++ backend/coin-control-backend/src/test/java/io/github/tekuan/coin_control_backend/CoinControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tekuan.coin_control_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
